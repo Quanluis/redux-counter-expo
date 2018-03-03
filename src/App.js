9c4be584b6895738076ac435a0e5a1563ee241c7
quan <quanluis@protonmail.com>
2018-03-02 19:48:30 -0500

    created map dispatch to props to provide increment and decrement to provide use of app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
        <h1>
          {this.props.count}
        </h1>
-       <button onClick = {() => this.props.dispatch({type: 'INCREMENT'}) } >+</button>
-       <button onClick = {() => this.props.dispatch({type: 'DECREMENT'})} >-</button>       
+       <button onClick = {this.props.increment} >+</button>
+       <button onClick = {this.props.decrement} >-</button>       
      </div>
     );
   }
@@ -22,4 +22,11 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, null)(App);
+const mapDispatchToProps = (dispatch) => {
+  return{
+    increment: () => dispatch({type: 'INCREMENT'}),
+    decrement: () => dispatch({type: 'DECREMENT'}),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

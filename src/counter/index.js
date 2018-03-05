68280b8a9f618ee3810b92a65a76a4e448f53b3b
quan <quanluis@protonmail.com>
2018-03-05 18:21:33 -0500

    created types file with increment and decrement and changed reducer to take in types

--- src/counter/index.js
+++ src/counter/index.js
@@ -1,12 +1,15 @@
+import * as types from './types';
+console.log(types)
+
 // initial state 
 const intitalState = 0;
 
 //reduce -> how state changes
 const countReducer = (state=intitalState, action) => {
    switch(action.type){
-     case "INCREMENT":
+     case types.INCREMENT:
      return state + 1;
-    case "DECREMENT":
+    case types.DECREMENT:
      return state - 1;
     default: 
       return state; 

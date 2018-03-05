6fe6021aa01b6e1f4e7714d8eb2bdc3a40bac726
quan <quanluis@protonmail.com>
2018-03-05 18:59:56 -0500

    added decrement by and increment by action

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import {connect} from 'react-redux';
 
 import Counter from './Counter';
-import {incrementAction, decrementAction} from './counter/actions'
+import {incrementAction,
+        decrementAction, 
+        incrementByAction, 
+        decrementByAction} from './counter/actions'
 
 
 class App extends Component {
@@ -19,7 +22,7 @@ class App extends Component {
 }
 const mapStateToProps = (state) => {
   return {
-    count: state
+    count: state.count
   }
 }
 
@@ -27,6 +30,8 @@ const mapDispatchToProps = (dispatch) => {
   return{
     increment: () => dispatch(incrementAction()),
     decrement: () => dispatch(decrementAction()),
+    incrementBy: (num) => dispatch(incrementByAction(num)),
+    decrementBy: (num) => dispatch(decrementByAction(num)),
   }
 }
 
--- src/counter/index.js
+++ src/counter/index.js
@@ -2,15 +2,32 @@ import * as types from './types';
 console.log(types)
 
 // initial state 
-const intitalState = 0;
+const intitalState = {
+    count: 0
+};
 
 //reduce -> how state changes
 const countReducer = (state=intitalState, action) => {
+    console.log(action);
    switch(action.type){
      case types.INCREMENT:
-     return state + 1;
+     return {...state,
+    count: state.count + 1 }
     case types.DECREMENT:
-     return state - 1;
+     return {
+         ...state,
+         count: state.count -1 
+     };
+     case types.DECREMENT_BY:
+     return{
+         ...state,
+         count: state.count - action.payload.value
+     };
+     case types.INCREMENT_BY:
+      return {
+          ...state,
+          count: state.count + action.payload.value
+      };
     default: 
       return state; 
    }
